e35cfc6f4cd30c75e8067d2ece4a375938d6a672
Nicolas Pastorio <npastoriodemoura@gamil.com>
2024-06-26 19:53:58 -0300

    Adicionado funções para conectar no banco de dados e salvar as tarefas

--- controllers/tarefaController.js
+++ controllers/tarefaController.js
@@ -1,13 +1,14 @@
 const Tarefa = require('../models/tarefa');
 const tarefas = [];
 
-function getTarefas(req, res){
+async function getTarefas(req, res){
+    tarefas = await Tarefa.listarTarefas();
     res.render('tarefas', {tarefas});
 }
 
-function addTarefa(req, res){
-    const { title } = req.body;
-    const tarefa = new Tarefa(Date.now(), title, false);
+async function addTarefa(req, res){
+    const { id, title } = req.body;
+    const tarefa = new Tarefa(id, title, false);
     tarefas.push(tarefa);
     res.redirect('tarefas');
 }
--- models/tarefa.js
+++ models/tarefa.js
@@ -4,6 +4,23 @@ class Tarefa{
         this.title = title;
         this.description = description;
     }
+
+    async salvar(){
+        const db = require("./database");
+        let sql = null;
+        if(this.id == ''){
+            sql = `INSERT INTO tarefa (title, description, usuario_id_usuario) VALUES ("${this.title}", "${this.description}", 1);`;
+        }else{
+            sql=``;
+        }
+        return await db.query(sql);
+    }
+
+    static async listarTarefas(){
+        const db = require('./database');
+        tarefas = await db.query(`SELECT * FROM tarefa`);
+        return tarefas;
+    }
 }
 
 module.exports = Tarefa;
