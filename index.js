faf7549e1e2f7460f345bdc60a287d3968d24b79
Nicolas Pastorio <npastoriodemoura@gamil.com>
2024-06-26 23:08:54 -0300

    Iniciando a autenticação de usuário

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const session = require('express-session');
 const tarefaController = require('./controllers/tarefaController');
+const usuarioController = require('./controllers/usuarioController');
+
 const app = express();
 const port = 3000;
+
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {res.send("<h1>API Tarefas</h1>")});
 app.use(express.urlencoded({ extended: true }));
 app.get('/tarefas', tarefaController.getTarefas);
 app.get('/tarefa/delete/:id', tarefaController.deleteTarefa);
+app.get('/login', usuarioController.login);
+app.use(session({secret: 'i1n2f3o4'}));
+app.get('/login', usuarioController.login);
+app.post('/usuario/autenticar', usuarioController.autenticar);
 app.post('/tarefas', tarefaController.addTarefa);
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
---
+++ models/usuarioModel.js
@@ -0,0 +1,11 @@
+class Usuario{
+    static async autenticar(email, senha){
+        const db = require('./database');
+        const md5 = require('md5');
+        const sql = `SELECT * FROM usuario WHERE amail = '${email}' AND senha = '${md5(senha)}';`;
+        console.log(sql);
+        return await db.query(sql)/
+    }
+}
+
+modules.exports = Usuario;
